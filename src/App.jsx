49e5f123534d3ec9dda868739d94ecfbab25f29d
Mira <kruzinka@yandex.ru>
2024-04-24 17:14:55 +0300

    testing api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
-function App() {
-  return (
-    <div>
-      AAAAAAAAAAAAAAAA
-    </div>
-  )
+const KEY = "1e11912acc74b0709e04f81e2455dc84";
+
+// работает только с VPN :c
+async function fetchAge() {
+  const { data } = await axios.get(
+    `https://api.themoviedb.org/3/movie/11?api_key=${KEY}`
+    // {
+    //   signal,
+    // }
+  );
+
+  return data.belongs_to_collection.name;
 }
 
-export default App
+function App() {
+  const { data, isLoading, isError } = useQuery(
+    ["film"],
+    () => fetchAge()
+    // {
+    //   enabled: false,
+    // }
+  );
+
+  console.log(data);
+
+  return <div>{data}</div>;
+}
+
+export default App;
