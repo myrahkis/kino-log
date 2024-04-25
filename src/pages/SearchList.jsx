e448316720880ebc51800abb6007d8994ebb60a0
Mira <kruzinka@yandex.ru>
2024-04-25 16:01:38 +0300

    styled search list

--- src/pages/SearchList.jsx
+++ src/pages/SearchList.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/prop-types */
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
+import styles from "./_searchList.module.scss";
 
 const KEY = "1e11912acc74b0709e04f81e2455dc84";
+const POSTER_PATH = "https://image.tmdb.org/t/p/original/";
 
 // работает только с VPN :c
 async function fetchSearch(signal, query) {
@@ -28,9 +30,9 @@ function SearchList({ query }) {
   );
 
   return (
-    <div>
+    <div className={styles["container"]}>
       <p>---результат поиска---</p>
-      <ul>
+      <ul className={styles["result-wrapper"]}>
         {data?.map((film, id) => (
           <FilmCard key={id} film={film} />
         ))}
@@ -41,11 +43,23 @@ function SearchList({ query }) {
 
 function FilmCard({ film }) {
   return (
-    <li style={{padding: '2rem'}}>
-      <img src={film.poster_path} alt=":(((" />
-      <h2>{film.original_title}</h2>
-      <p>{film.overview}</p>
-      <p>{film.release_date}</p>
+    <li className={styles["card"]}>
+      <img
+        className={styles["card__poster"]}
+        src={
+          film.poster_path
+            ? `${POSTER_PATH}${film.poster_path}`
+            : "image_not_available.png"
+        }
+        alt="Не удалось загрузить постер."
+      />
+      <div className={styles['card__info-wrapper']}>
+        <h2 className={styles["card__title"]}>{film.original_title}</h2>
+        {film.overview && (
+          <p className={styles["card__overview"]}>{film.overview}</p>
+        )}
+        <p className={styles["card__release"]}>Дата выхода: {film.release_date}</p>
+      </div>
     </li>
   );
 }
