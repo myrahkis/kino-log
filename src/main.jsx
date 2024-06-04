65d26aa0923d22bf4b295e298c6c6c863e2766dd
Mira <kruzinka@yandex.ru>
2024-06-04 19:28:03 +0300

    changed path

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/vk-test-films">
+    <BrowserRouter basename="/kinolog">
       <App />
     </BrowserRouter>
   </React.StrictMode>
